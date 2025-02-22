8727f860dc65790b333ec0cb0aeeba98c3c9c644
Shane Dunne <77242413+shanedunne@users.noreply.github.com>
2025-02-22 12:46:12 +0000

    add routes for user and location api

---
+++ src/api-routes.js
@@ -0,0 +1,16 @@
+import { locationApi } from "./api/location-api.js";
+import { userApi } from "./api/user-api.js";
+
+export const apiRoutes = [
+    // user api routes
+    { method: "GET", path: "/api/users", config: userApi.find },
+    { method: "POST", path: "/api/users", config: userApi.create },
+    { method: "DELETE", path: "/api/users", config: userApi.deleteAll },
+    { method: "GET", path: "/api/users/{id}", config: userApi.findOne },
+
+    // location api routes
+    { method: "GET", path: "/api/locations", config: locationApi.find },
+    { method: "POST", path: "/api/locations", config: locationApi.create },
+    { method: "DELETE", path: "/api/locations", config: locationApi.deleteAll },
+    { method: "GET", path: "/api/locations/{id}", config: locationApi.findOne },
+];
