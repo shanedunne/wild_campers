8bd031e7f0a8111e06ba2594b8b7a0d6b8a13e35
Shane Dunne <77242413+shanedunne@users.noreply.github.com>
2025-02-24 13:04:24 +0000

    add ability to add lat, long, description for location

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,35 +1,42 @@
 import { db } from "../models/db.js";
 
 export const dashboardController = {
-    index: {
-      handler: async function (request, h) {
-        const locations = await db.locationStore.getAllLocations();
-        console.log(locations)
+  index: {
+    handler: async function (request, h) {
+      const locations = await db.locationStore.getAllLocations();
+      console.log(locations)
 
-        const viewData = {
-          title: "Wild Campers Dashboard",
-          locations: locations
-        }
-        return h.view("dashboard-view", viewData);
+      const viewData = {
+        title: "Wild Campers Dashboard",
+        locations: locations
+      }
+      return h.view("dashboard-view", viewData);
+    },
+  },
+
+  addLocation: {
+    handler: async function (request, h) {
+      const locationName = request.payload.locationName;
+      const latitude = request.payload.latitude;
+      const longitude = request.payload.longitude;
+      const locationDescription = request.payload.locationDescription;
+      await db.locationStore.addLocation({
+        name: locationName,
+        latitude: latitude,
+        longitude: longitude,
+        locationDescription: locationDescription
+      });
+      return h.redirect("/dashboard");
+    },
+    validate: {
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        return h.view("dashboard-view", {
+          title: 'Error adding a location',
+          errors: error.details
+        }).takeover().code(400);
       },
     },
+  },
 
-    addLocation: {
-      handler: async function (request, h) {
-        const locationName = request.payload.locationName;
-        await db.locationStore.addLocation({ name: locationName });
-        return h.redirect("/dashboard");
-      },
-      validate: {
-        options: { abortEarly: false },
-        failAction: function (request, h, error) {
-          return h.view("dashboard-view", {
-            title: 'Error adding a location',
-            errors: error.details
-          }).takeover().code(400);
-        },
-      },
-    },
-    
-  };
-  
+};
