886c506904830476896dfcf8017f1d483e141421
Shane Dunne <77242413+shanedunne@users.noreply.github.com>
2025-03-06 19:47:45 +0000

    add test fixtures for users and locations

---
+++ test/fixtures.js
@@ -0,0 +1,60 @@
+export const serviceUrl = "http://localhost:3000";
+
+
+export const maggie = {
+    firstName: "Maggie",
+    lastName: "Simpson",
+    email: "maggie@simpson.com",
+    password: "secret"
+};
+
+export const testUsers = [
+    {
+        firstName: "Homer",
+        lastName: "Simpson",
+        email: "homer@simpson.com",
+        password: "secret"
+    },
+    {
+        firstName: "Marge",
+        lastName: "Simpson",
+        email: "marge@simpson.com",
+        password: "secret"
+    },
+    {
+        firstName: "Bart",
+        lastName: "Simpson",
+        email: "bart@simpson.com",
+        password: "secret"
+    }
+];
+
+export const testLocations = [
+    {
+        name: "Wicklow Mountains, Ireland",
+        latitude: "53.0000",
+        longitude: "-6.4000",
+        locationDescription: "Experience rugged landscapes and serene valleys ideal for camping and hiking."
+    },
+    {
+        name: "Killarney National Park, Ireland",
+        latitude: "52.0000",
+        longitude: "-9.5000",
+        locationDescription: "Lush woodlands and sparkling lakes make this park perfect for a nature getaway."
+    },
+    {
+        name: "Glendalough, Ireland",
+        latitude: "53.0010",
+        longitude: "-6.3290",
+        locationDescription: "Discover ancient monastic ruins set amid tranquil scenery, perfect for a peaceful camping retreat."
+    },
+
+];
+
+export const location =
+{
+    name: "Achill Island, Ireland",
+    latitude: "53.9000",
+    longitude: "-10.2000",
+    locationDescription: "Enjoy dramatic coastal views and rugged terrain on Ireland's largest island, ideal for outdoor adventures."
+}
