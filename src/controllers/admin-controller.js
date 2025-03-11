726e8413d4c4ff98a822fa6a1cdadc193e5ebabf
Shane Dunne <77242413+shanedunne@users.noreply.github.com>
2025-03-11 15:50:10 +0000

    add admin-controller with index, deleteCategory and deleteUser functions

---
+++ src/controllers/admin-controller.js
@@ -0,0 +1,70 @@
+import { db } from "../models/db.js";
+
+export const adminController = {
+    // render admin view listing users and categories, with ability to add a category
+    index: {
+        handler: async function (request, h) {
+            const users = await db.userStore.getAllUsers();
+            const categories = await db.categoryStore.getAllCategories();
+
+            const viewData = {
+                title: "Admin Dashboard",
+                users: users,
+                categories: categories
+            }
+            return h.view("admin-view", viewData);
+        },
+    },
+
+    // function to add category
+    addCategory: {
+        validate: {
+            options: { abortEarly: false },
+            payload: CategorySpec,
+            failAction: function (request, h, error) {
+                return h.view("admin-view", { title: "Error adding category", errors: error.details }).takeover().code(400);
+            },
+        },
+
+        handler: async function (request, h) {
+            const category = request.payload.categoryName;
+            await db.categoryStore.addCategory({
+                category: category,
+            });
+            return h.redirect("/admin-view");
+        },
+    },
+
+    // function to delete category
+    deleteCategory: {
+        validate: {
+            options: { abortEarly: false },
+            failAction: function (request, h, error) {
+                return h.view("admin-view", { title: "Error deleting category", errors: error.details }).takeover().code(400);
+            },
+        },
+
+        handler: async function (request, h) {
+            const category_id = request.params.id;
+            await db.categoryStore.deleteCategory(category_id);
+            return h.redirect("/admin-view");
+        },
+    },
+
+     // function to delete user
+     deleteUser: {
+        validate: {
+            options: { abortEarly: false },
+            failAction: function (request, h, error) {
+                return h.view("admin-view", { title: "Error deleting user", errors: error.details }).takeover().code(400);
+            },
+        },
+
+        handler: async function (request, h) {
+            const user_id = request.params.id;
+            await db.userStore.deleteUser(user_id);
+            return h.redirect("/admin-view");
+        },
+    },
+
+};
