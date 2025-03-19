dee8a14d63801945df54b817daa8d13f2e61e2fa
Shane Dunne <77242413+shanedunne@users.noreply.github.com>
2025-03-19 10:43:29 +0000

    add api app service script to test with axios

---
+++ test/api/api-app-service.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+import { serviceUrl } from "../fixtures.js";
+
+export const playtimeService = {
+  serviceUrl: serviceUrl,
+
+  async createUser(user) {
+    const res = await axios.post(`${this.serviceUrl}/api/users`, user);
+    return res.data;
+  },
+
+  async getUser(id) {
+    const res = await axios.get(`${this.serviceUrl}/api/users/${id}`);
+    return res.data;
+  },
+
+  async getAllUsers() {
+    const res = await axios.get(`${this.serviceUrl}/api/users`);
+    return res.data;
+  },
+
+  async deleteAllUsers() {
+    const res = await axios.delete(`${this.serviceUrl}/api/users`);
+    return res.data;
+  },
+
+  async createCategory(category) {
+    const res = await axios.post(`${this.serviceUrl}/api/categories`, category);
+    return res.data;
+  },
+
+  async deleteAllCategories() {
+    const response = await axios.delete(`${this.serviceUrl}/api/categories`);
+    return response.data;
+  },
+
+  async deleteCategory(id) {
+    const response = await axios.delete(`${this.serviceUrl}/api/categories/${id}`);
+    return response;
+  },
+
+  async getAllCategoies() {
+    const res = await axios.get(`${this.serviceUrl}/api/categories`);
+    return res.data;
+  },
+
+  async getCategory(id) {
+    const res = await axios.get(`${this.serviceUrl}/api/categories/${id}`);
+    return res.data;
+  },
+
+  async getAllLocations() {
+    const res = await axios.get(`${this.serviceUrl}/api/locations`);
+    return res.data;
+  },
+
+  async createLocation(id, location) {
+    const res = await axios.post(`${this.serviceUrl}/api/categorys/${id}/locations`, location);
+    return res.data;
+  },
+
+  async deleteAllLocations() {
+    const res = await axios.delete(`${this.serviceUrl}/api/locations`);
+    return res.data;
+  },
+
+  async getLocation(id) {
+    const res = await axios.get(`${this.serviceUrl}/api/locations/${id}`);
+    return res.data;
+  },
+
+  async deleteLocation(id) {
+    const res = await axios.delete(`${this.serviceUrl}/api/locations/${id}`);
+    return res.data;
+  },
+};
--- test/fixtures.js
+++ test/fixtures.js
@@ -72,3 +72,4 @@ export const testCategories = [
     { categoryName: "Mountain" },
     { categoryName: "River" },
 ]
+
